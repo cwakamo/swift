099272e1b0fbbf432a8e8d5b4a75136184fba5e2
Connor Wakamo <cwakamo@apple.com>
2021-12-04 12:22:25 -0800

    WIP

--- include/swift-c/Compiler/Compiler.h
+++ include/swift-c/Compiler/Compiler.h
@@ -12,15 +12,119 @@
 
 #include "CompilerMacros.h"
 
+#include <stddef.h>
+#include <stdint.h>
+
 /// The version constants for the SwiftCompiler C API.
 /// SWIFTCOMPILER_VERSION_MINOR should increase when there are API additions.
-/// SWIFTCOMPILER_VERSION_MAJOR is intended for "major" source/ABI breaking changes.
+/// SWIFTCOMPILER_VERSION_MAJOR is intended for "major" source/ABI breaking
+/// changes.
 #define SWIFTCOMPILER_VERSION_MAJOR 0
 #define SWIFTCOMPILER_VERSION_MINOR 1
 
 SWIFTCOMPILER_BEGIN_DECLS
-  
+
+//--- ---//
+
+typedef struct swift_compiler_diagnostic_s * swift_compiler_diagnostic_t;
+typedef void(*swift_compiler_diagnostic_handler_t)(swift_compiler_diagnostic_t);
+
+typedef struct {
+  const char *file;
+  size_t line;
+  size_t column;
+} swift_compiler_diagnostic_location_t;
+
+typedef struct {
+  swift_compiler_diagnostic_location_t location;
+  size_t length;
+} swift_compiler_diagnostic_range_t;
+
+typedef struct {
+  swift_compiler_diagnostic_range_t range;
+  const char *replacement_text;
+} swift_compiler_diagnostic_fixit_t;
+
+typedef enum {
+  swift_compiler_diagnostic_severity_error,
+  swift_compiler_diagnostic_severity_warning,
+  swift_compiler_diagnostic_severity_remark,
+  swift_compiler_diagnostic_severity_note,
+} swift_compiler_diagnostic_severity_t;
+
 SWIFTCOMPILER_PUBLIC
-int swift_compiler_invoke(int argc, const char **argv);
+swift_compiler_diagnostic_location_t
+swift_compiler_diagnostic_get_location(swift_compiler_diagnostic_t diagnostic);
+
+SWIFTCOMPILER_PUBLIC
+swift_compiler_diagnostic_severity_t
+swift_compiler_diagnostic_get_severity(swift_compiler_diagnostic_t diagnostic);
+
+SWIFTCOMPILER_PUBLIC
+const char *
+swift_compiler_diagnostic_get_message(swift_compiler_diagnostic_t diagnostic);
+
+SWIFTCOMPILER_PUBLIC
+const char *
+swift_compiler_diagnostic_get_format_string(
+  swift_compiler_diagnostic_t diagnostic);
+
+SWIFTCOMPILER_PUBLIC
+const char *
+swift_compiler_diagnostic_get_message_with_custom_format_string(
+  swift_compiler_diagnostic_t diagnostic, const char *format_string);
+
+SWIFTCOMPILER_PUBLIC
+void
+swift_compiler_diagnostic_get_ranges(swift_compiler_diagnostic_t diagnostic,
+  size_t *outCount, swift_compiler_diagnostic_range_t **outRanges);
+
+SWIFTCOMPILER_PUBLIC
+void
+swift_compiler_diagnostic_get_fixits(swift_compiler_diagnostic_t diagnostic,
+  size_t *outCount, swift_compiler_diagnostic_fixit_t **outFixits);
+
+SWIFTCOMPILER_PUBLIC
+uint64_t
+swift_compiler_diagnostic_get_id(swift_compiler_diagnostic_t diagnostic);
+
+//--- ---//
+
+typedef struct swift_compiler_delegate_s * swift_compiler_delegate_t;
+
+SWIFTCOMPILER_PUBLIC
+swift_compiler_delegate_t swift_compiler_delegate_create(void);
+
+SWIFTCOMPILER_PUBLIC
+void swift_compiler_delegate_dispose(swift_compiler_delegate_t delegate);
+
+SWIFTCOMPILER_PUBLIC
+void swift_compiler_delegate_set_diagnostic_handler(
+  swift_compiler_delegate_t delegate,
+  swift_compiler_diagnostic_handler_t diagnosticHandler);
+
+//--- ---//
+
+typedef struct swift_compiler_invocation_s * swift_compiler_invocation_t;
+
+SWIFTCOMPILER_PUBLIC
+swift_compiler_invocation_t swift_compiler_invocation_create(void);
+
+SWIFTCOMPILER_PUBLIC
+void swift_compiler_invocation_dispose(swift_compiler_invocation_t invocation);
+
+SWIFTCOMPILER_PUBLIC
+void swift_compiler_invocation_set_frontend_arguments(
+  swift_compiler_invocation_t invocation, int count, const char **args);
+
+SWIFTCOMPILER_PUBLIC
+void swift_compiler_invocation_add_source_buffer(
+  swift_compiler_invocation_t invocation, const char *file, const char *buffer);
+
+//--- ---//
+
+SWIFTCOMPILER_PUBLIC
+int swift_compiler_invokeFrontend(swift_compiler_invocation_t invocation,
+                                  swift_compiler_delegate_t delegate);
   
 SWIFTCOMPILER_END_DECLS
--- tools/libSwiftCompiler/libSwiftCompiler.cpp
+++ tools/libSwiftCompiler/libSwiftCompiler.cpp
@@ -14,9 +14,120 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "swift/DriverTool/DriverTool.h"
+#include "swift/AST/DiagnosticConsumer.h"
+#include "swift/AST/DiagnosticEngine.h"
+#include "swift/AST/DiagnosticsFrontend.h"
+#include "swift/Basic/LLVM.h"
+#include "swift/Basic/InitializeLibSwift.h"
+#include "swift/Frontend/Frontend.h"
+#include "swift/FrontendTool/FrontendTool.h"
 #include "swift-c/Compiler/Compiler.h"
 
-int swift_compiler_invoke(int argc, const char **argv) {
-  return swift::mainEntry(argc, argv);
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/ADT/StringRef.h"
+
+#include <vector>
+#include <string>
+
+using namespace swift;
+
+struct swift_compiler_delegate_s {
+  swift_compiler_diagnostic_handler_t diagnosticHandler;
+};
+
+swift_compiler_delegate_t swift_compiler_delegate_create(void) {
+  return new swift_compiler_delegate_s;
+}
+
+void swift_compiler_delegate_dispose(swift_compiler_delegate_t delegate) {
+  delete delegate;
+}
+
+void swift_compiler_delegate_set_diagnostic_handler(
+  swift_compiler_delegate_t delegate,
+  swift_compiler_diagnostic_handler_t diagnosticHandler) {
+  delegate->diagnosticHandler = diagnosticHandler;  
+}
+
+struct swift_compiler_invocation_s {
+  SmallVector<std::string, 256> Args;
+};
+
+swift_compiler_invocation_t swift_compiler_invocation_create(void) {
+  return new swift_compiler_invocation_s;
+}
+
+void swift_compiler_invocation_dispose(swift_compiler_invocation_t invocation) {
+  delete invocation;
+}
+
+void swift_compiler_invocation_set_frontend_arguments(
+  swift_compiler_invocation_t invocation,
+  int count, const char **args) {
+  invocation->Args.clear();
+  for (int i = 0; i < count; ++i) {
+    invocation->Args.push_back(args[i]);
+  }
+}
+
+void swift_compiler_invocation_set_driver_arguments(
+  swift_compiler_invocation_t invocation,
+  int count, const char **args) {
+  swift_compiler_invocation_set_frontend_arguments(invocation, count, args);
+}
+
+namespace swift {
+namespace compilerlib {
+class DiagnosticConsumer : public swift::DiagnosticConsumer {
+public:
+  virtual void handleDiagnostic(SourceManager &SM,
+                                const DiagnosticInfo &Info) override;
+};
+  
+class FrontendObserver : public swift::FrontendObserver {
+private:
+  DiagnosticConsumer *CLDC;
+public:
+  FrontendObserver(DiagnosticConsumer *CLDC) : CLDC(CLDC) {};
+  ~FrontendObserver() = default;
+  
+  void configuredCompiler(CompilerInstance &instance) override;
+}; 
+}
+}
+
+void compilerlib::DiagnosticConsumer::handleDiagnostic(SourceManager &SM,
+  const DiagnosticInfo &Info)
+{
+  abort();
+}
+
+void compilerlib::FrontendObserver::configuredCompiler(
+  CompilerInstance &instance)
+{
+  if (CLDC) {
+    // Add our custom DiagnosticConsumer to the CompilerInstance.
+    instance.getDiags().addConsumer(*CLDC);
+  }
+}
+
+int swift_compiler_invokeFrontend(swift_compiler_invocation_t invocation,
+                                  swift_compiler_delegate_t delegate)
+{
+  // This is done here and not done in FrontendTool.cpp, because
+  // FrontendTool.cpp is linked to tools, which don't use libswift.
+  initializeLibSwift();
+ 
+  SmallVector<const char *, 256> argv;
+  for (auto &arg : invocation->Args) {
+    argv.push_back(arg.c_str());
+  }
+  
+  compilerlib::DiagnosticConsumer CLDC;
+  std::unique_ptr<compilerlib::FrontendObserver> Observer(
+    new compilerlib::FrontendObserver(&CLDC));
+  
+  return performFrontend(argv, "swift-frontend",
+    (void *)&swift_compiler_invokeFrontend, Observer.get());
 }
